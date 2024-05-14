11d90524c8ef0a875b92a399cd5bcd8c48037eec
Jakub Kozłowski - laptop <s197623@student.pg.edu.pl>
2024-05-14 17:29:01 +0200

    opisy do pythona

--- cmake_example/python.py
+++ cmake_example/python.py
@@ -11,10 +11,13 @@ print(graf())
 #print(sinus())
 
 
-audio_data = readAudioFile("cmake_example\CantinaBand3.wav")
-print(audio_data)
-print(visualizeAudio(audio_data))
 
-#print(graf())
+
+
+audio_data = readAudioFile("cmake_example\CantinaBand3.wav")# Wczytanie do pythona pliku dźwiękowego CantinaBand3.wav
+print(audio_data) #wyświetlenie wartości poszczególnych sampli
+print(visualizeAudio(audio_data)) #wizualizacja za pomocą Gnuplota
+
+
 # ss Szymon generowanie sygnałów sin cos itd, rozmycie gaussa 
 # Jakub filtracja 1D i 2D 
